655f099f8104e8ce0b4d2bf4f1b2d39bc0c6d9da
rlecheta <rlecheta@livetouch.com.br>
2020-08-29 21:17:55 -0300

    hello

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.helloandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.helloandroid.extensions.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val btEnviar = findViewById<Button>(R.id.btEnviar)
+        btEnviar.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        val msg = tMensagem.text.toString()
+
+        tMensagemFinal.text = "Olá $msg"
     }
 }
