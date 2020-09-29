684ab725086c45ca42aa4e7c446797374c10b35f
rlecheta <rlecheta@livetouch.com.br>
2020-09-29 10:41:58 -0300

    .setOnClickListener

--- Uninter/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ Uninter/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -15,6 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btLogin).setOnClickListener {
+            onClickLogin()
+        }
+
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener {
+            onClickEsqueciSenha()
+        }
+
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener {
+            onClickCadastrar()
+        }
     }
 
     private fun onClickLogin() {
