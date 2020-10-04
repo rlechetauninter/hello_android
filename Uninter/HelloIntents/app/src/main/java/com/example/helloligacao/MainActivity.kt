687c5ca7a79a14e93fc83bddb8f81ef1f59aa131
rlecheta <rlecheta@livetouch.com.br>
2020-10-03 21:54:57 -0300

    ok

--- Uninter/HelloIntents/app/src/main/java/com/example/helloligacao/MainActivity.kt
+++ Uninter/HelloIntents/app/src/main/java/com/example/helloligacao/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.helloligacao
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btTeste.setOnClickListener {
-            onClickTeste()
-        }
-    }
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CALL_PHONE),
+            1
+        )
 
-    private fun onClickTeste() {
-        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:987654321"))
-        startActivity(intent)
+        btTeste.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:987654321"))
+            startActivity(intent)
+
+        }
     }
 }
