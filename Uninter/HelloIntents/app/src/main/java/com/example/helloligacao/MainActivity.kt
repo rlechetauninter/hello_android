b296f5d856351580662fea9c034cb5f168888f50
rlecheta <rlecheta@livetouch.com.br>
2020-10-03 23:10:19 -0300

    ok

--- Uninter/HelloIntents/app/src/main/java/com/example/helloligacao/MainActivity.kt
+++ Uninter/HelloIntents/app/src/main/java/com/example/helloligacao/MainActivity.kt
@@ -4,8 +4,9 @@ import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,16 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.CALL_PHONE),
-            1
-        )
+        // (1): Esse método valida se permissão já foi concedida.
+        // Caso o usuário já tenha aceitado, ele retorna true.
+        // Caso contrário, retorna false e mostra o alerta ao usuário.
+        val ok = PermissionUtils.request(this, arrayOf(Manifest.permission.CALL_PHONE))
 
-        btTeste.setOnClickListener {
-            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:987654321"))
-            startActivity(intent)
+        if(ok) {
+            btTeste.setOnClickListener {
+                val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:987654321"))
+                startActivity(intent)
+            }
+        } else {
+            // Se o usuário ainda não aceitou a permissão ou se ele foi negada..
+            // Deixa o botão Invisível
+            btTeste.visibility = View.INVISIBLE
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val ok = PermissionUtils.validate(this, permissions)
+        if(ok) {
+            // Deixa o botão visível se o usuário aceitou a permissão
+            btTeste.visibility = View.VISIBLE
         }
     }
 }
--- Uninter/HelloIntents/app/src/main/java/com/example/helloligacao/PermissionUtils.kt
+++ Uninter/HelloIntents/app/src/main/java/com/example/helloligacao/PermissionUtils.kt
@@ -1,59 +1,32 @@
 package com.example.helloligacao
 
-import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.*
 
 object PermissionUtils {
-    /*fun request(activity: Activity) {
-        if (!
-            checkGpsPermissionsGranted(activity)) {
+    // Solicita uma permissão se ainda não foi concedida
+    fun request(activity: Activity, permissions: Array<String>) : Boolean {
+        val ok = validate(activity, permissions)
+        if (!ok) {
             ActivityCompat.requestPermissions(
                 activity,
-                gpsPermissions,
+                permissions,
                 1
             )
         }
+        return ok
     }
-
-    val gpsPermissions: Array<String>
-        get() {
-            val permissions: MutableList<String> =
-                ArrayList()
-            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
-            permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                permissions.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-            }
-            val array = arrayOfNulls<String>(permissions.size)
-            return permissions.toArray<String>(array)
-        }
-
-    fun validatePermissions(context: Context?): Boolean {
-
+    // Valida se as permissões foram concedidas
+    fun validate(context: Context, permissions: Array<String>): Boolean {
         for (permission in permissions) {
-            val result = ContextCompat.checkSelfPermission(
-                context!!,
-                permission
-            )
+            val result = ContextCompat.checkSelfPermission(context, permission)
             if (result != PackageManager.PERMISSION_GRANTED) {
                 return false
             }
         }
         return true
     }
-
-    fun validateRequestResults(grantResults: IntArray): Boolean {
-        for (result in grantResults) {
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                return false
-            }
-        }
-        return true
-    }*/
 }
